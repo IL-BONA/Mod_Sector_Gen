55ff8ee01ebf44ef271c82d9edd1442e8a98ca64
Luca Caruso <luca+github@enricocaruso.net>
2025-05-08 21:22:02 +0200

    Test

---
+++ src/modTest/Main.java
@@ -0,0 +1,26 @@
+package modTest;
+
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class Main extends Mod {
+
+    public Main() {
+        Log.info("Loading Test Mod...");
+    }
+
+    @Override
+    public void init() {
+        Log.info("Initializing Test Mod...");
+        try {
+            // Add your initialization code here
+        } catch(Exception e) {
+            Log.err("Test Mod failed to initialize", e);
+        }
+    }
+
+    @Override
+    public void loadContent() {
+        Log.info("Loading Test Mod content...");
+    }
+}
